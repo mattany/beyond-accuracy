fe6ea4596ac4cf5a223fd50eabe6e582cc99fb44
Mattan Yeroushalmi <mattan.yeroushalmi@similarweb.com>
2024-10-12 23:26:57 +0300

    add questions to joined rag csv

--- Benchmarking/deep_eval/RAG/join_answers.py
+++ Benchmarking/deep_eval/RAG/join_answers.py
@@ -6,12 +6,13 @@ gpt_4o_df = pd.read_csv(f'{data_dir}/gpt_4o_answers.csv').rename(columns={'answe
 gpt_4o_df2 = pd.read_csv(f'{data_dir}/gpt_4o_validation_run.csv').rename(columns={'answer': 'gpt_4o_2'})
 llama2_df = pd.read_csv(f'{data_dir}/llama2_basemodel_answers.csv').rename(columns={'answer': 'llama_2'})
 llama2_sft_df = pd.read_csv(f'{data_dir}/llama2_sft_answers.csv').rename(columns={'answer': 'llama_2_sft'})
-
-joined_df = (gpt_4_turbo_df
+questions = pd.read_csv(f'{data_dir}/question_bank_manually_selected.csv')
+joined_df = (questions
              .merge(gpt_4o_df, on="Index")
              .merge(gpt_4o_df2, on="Index")
              .merge(llama2_df, on="Index")
              .merge(llama2_sft_df, on="Index")
+             .merge(gpt_4_turbo_df, on="Index")
              .drop(columns=['Index'])
              )
 
