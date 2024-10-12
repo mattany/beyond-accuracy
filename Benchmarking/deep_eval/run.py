a4c5c65adbd474f790227201f31dd184fa03bb86
Mattan Yeroushalmi <mattan.yeroushalmi@similarweb.com>
2024-10-12 23:29:19 +0300

    adapt metric generation for correctness

--- Benchmarking/deep_eval/run.py
+++ Benchmarking/deep_eval/run.py
@@ -1,58 +1,37 @@
 import os
-
 from tqdm import tqdm
-
 from Jargon.jargon_metric import JargonMetric
+from config import PROJECT_DIR
 
 OPENAI_API_KEY = "sk-proj-4reyI857Dx1FXwMAjCtCT3BlbkFJVrdDBRixPZCAHIfntrKN"
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-#
+
 import pandas as pd
 from deepeval.test_case import LLMTestCase
+from g_eval.zemla_metrics import internal_coherence_metric, completeness_metric, alternatives_metric, \
+    articulation_metric, perceived_truth_metric
 # from g_eval.zemla_metrics import completeness_metric
 from g_eval.explanation_quality import explanation_type_metric, correctness_metric, metaphor_metric, \
     content_units_metric, connection_to_everyday_life_metric, humor_metric, analogy_metric
 
 
-#"~/studies/thesis/Benchmarking/deep_eval/DPO_data/llama3_18B_ask_science_answers.csv"
-# def get_or_create_eval_dataset():
-#     try:
-#         df = pd.read_csv("~/studies/thesis/Benchmarking/deep_eval/data/evaluation_dataset.csv")
-#         return df
-#     except FileNotFoundError:
-#         pass
-#     llama_df = pd.read_csv("~/studies/thesis/Benchmarking/deep_eval/data/llama7b_base_vs_sft.csv")
-#     print("LLAMA BEFORE")
-#     print(llama_df.dtypes)
-#
-#     llama_df['question'] = llama_df['question'].astype(str)
-#     print("LLAMA AFTER")
-#
-#     print(llama_df.dtypes)
-#
-#     df = pd.read_csv("~/studies/thesis/Benchmarking/deep_eval/DPO_data/llama3_18B_ask_science_answers.csv")
-#     print("DF BEFORE")
-#
-#     print(df.dtypes)
-#     df['question'] = df['question'].astype(str)
-#     print("DF AFTER")
-#     print(df.dtypes)
-#
-#
-#     eval_df = pd.merge(df, llama_df, on='question', how='inner')
-#     eval_df.to_csv("~/studies/thesis/Benchmarking/deep_eval/data/evaluation_dataset.csv", index=False)
-#     return eval_df
-# ### eval df has the answer from llama 3.1, llama 2 7b, and the fine tuned llama 2 7b
-
-
-def update_or_insert_score_column(eval_df, output_path, answer_column, model_name, metric_function, metric_name):
+def update_or_insert_score_column(eval_df, output_path, answer_column, model_name, metric_function, metric_name, reference_column=None):
     scores = []
     reasons = []
+    if reference_column:
+        metric_name = f"{metric_name}_with_reference_{reference_column}"
     for index, row in tqdm(eval_df.iterrows(), total=eval_df.shape[0]):
-        test_case = LLMTestCase(
-            input=row['question'],
-            actual_output=row[answer_column],
-        )
+        if reference_column:
+            test_case = LLMTestCase(
+                input=row['question'],
+                actual_output=row[answer_column],
+                expected_output=row[reference_column]
+            )
+        else:
+            test_case = LLMTestCase(
+                input=row['question'],
+                actual_output=row[answer_column],
+            )
         metric_function.measure(test_case)
         print("Question:", row['question'])
         print(f"{answer_column}:", row[answer_column])
@@ -76,41 +55,85 @@ def update_or_insert_score_column(eval_df, output_path, answer_column, model_nam
     # print("\n")
 
 
-def generate_metric_report(metrics, evaluation_dataset, model_map):
+def generate_metric_report(metrics, evaluation_dataset, model_map, reference_models=None, models_to_evaluate=None):
+    if not models_to_evaluate:
+        models_to_evaluate = model_map.keys()
     eval_df = pd.read_csv(evaluation_dataset)
-    # for i in range(3):
     for metric, metric_function in metrics.items():
-        for model, answer_column in model_map.items():
-            print("Evaluating", metric, "for", model)
-            update_or_insert_score_column(
-                eval_df,
-                output_path=f"/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/deep_eval/data/{metric}_evaluation_scores_run_0.csv",
-                answer_column=answer_column,
-                model_name=model,
-                metric_function=metric_function,
-                metric_name=metric,
-            )
+        for model in models_to_evaluate:
+            answer_column = model_map[model]
+            if reference_models:
+                for reference_model in reference_models:
+                    if reference_model == model:
+                        continue
+                    print("Evaluating", metric, "for", model, "with reference model", reference_model)
+                    update_or_insert_score_column(
+                        eval_df,
+                        output_path=f"/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/deep_eval/data/{metric}_evaluation_scores_run_0.csv",
+                        answer_column=answer_column,
+                        model_name=model,
+                        metric_function=metric_function,
+                        metric_name=metric,
+                        reference_column=model_map[reference_model]
+                    )
+            else:
+                print("Evaluating", metric, "for", model)
+                update_or_insert_score_column(
+                    eval_df,
+                    output_path=f"/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/deep_eval/data/{metric}_evaluation_scores_run_0.csv",
+                    answer_column=answer_column,
+                    model_name=model,
+                    metric_function=metric_function,
+                    metric_name=metric
+                )
 
 
 if __name__ == "__main__":
+    # RAG
     generate_metric_report(
         metrics={
-            # 'jargon': JargonMetric(),
-            # 'metaphor': metaphor_metric,
-            # 'explanation_type': explanation_type_metric,
-            'content_units': content_units_metric,
-            # 'connection_to_everyday_life': connection_to_everyday_life_metric,
-            # 'humor': humor_metric,
-            # 'analogy': analogy_metric
+            'correctness': correctness_metric
         },
-        evaluation_dataset="~/studies/thesis/Benchmarking/deep_eval/data/evaluation_dataset.csv",
+        evaluation_dataset=f'{PROJECT_DIR}/Benchmarking/deep_eval/RAG/data/joined_answers.csv',
         model_map={
-            # 'llama_2_sft': 'sft_model_answer',
-            # 'llama_2_base': 'base_model_answer',
-            # 'llama_3_1': 'llama3_1_instruct_answer',
-            # 'gpt_3.5_turbo': 'gpt_3_5_outputs',
-            'gpt_4o': 'gpt_4o_outputs',
-            'gpt_3_5_cot': 'gpt_3_5_cot',
-            'gpt_4': 'gpt_4_outputs'
-        }
+            'gpt_4_turbo': 'gpt_4_turbo',
+            'gpt_4o': 'gpt_4o_1',
+            'gpt_4o_validation': 'gpt_4o_2',
+            'llama_2_base': 'llama_2',
+            'llama_2_sft': 'llama_2_sft',
+        },
+        reference_models=['gpt_4o'],
+        models_to_evaluate=['llama_2_sft', 'gpt_4o_validation', 'gpt_4_turbo', 'llama_2_base']
     )
+
+    # TEST SET
+    # generate_metric_report(
+    #     metrics={
+    #         ## BARAM TSABARI METRICS
+    #         # 'jargon': JargonMetric(),
+    #         # 'explanation_type': explanation_type_metric,
+    #         # 'metaphor': metaphor_metric,
+    #         # 'content_units': content_units_metric,
+    #         # 'connection_to_everyday_life': connection_to_everyday_life_metric,
+    #         # 'humor': humor_metric,
+    #         # 'analogy': analogy_metric
+    #         ## ZEMLA METRICS
+    #         # 'internal_coherence': internal_coherence_metric,
+    #         # 'completeness': completeness_metric,
+    #         # 'alternatives': alternatives_metric,
+    #         # 'articulation': articulation_metric,
+    #         # 'perceived_truth': perceived_truth_metric
+    #         ## CORRECTNESS METRICS
+    #         'correctness': correctness_metric
+    #     },
+    #     evaluation_dataset="~/studies/thesis/Benchmarking/deep_eval/data/evaluation_dataset.csv",
+    #     model_map={
+    #         'llama_2_sft': 'sft_model_answer',
+    #         'llama_2_base': 'base_model_answer',
+    #         'llama_3_1': 'llama3_1_instruct_answer',
+    #         'gpt_3.5_turbo': 'gpt_3_5_outputs',
+    #         'gpt_4o': 'gpt_4o_outputs',
+    #         'gpt_3_5_cot': 'gpt_3_5_cot',
+    #         'gpt_4': 'gpt_4_outputs'
+    #     }
+    # )
