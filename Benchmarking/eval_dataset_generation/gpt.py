b38fb543566e5668ccfbfe14aa8b670e644a39de
Mattan Yeroushalmi <mattan.yeroushalmi@similarweb.com>
2024-10-12 21:18:14 +0300

    add gpt 3.5 cot answers

--- Benchmarking/eval_dataset_generation/gpt.py
+++ Benchmarking/eval_dataset_generation/gpt.py
@@ -4,11 +4,17 @@ from SFT.batch_file_gen.gen_batch import create_input_batch_files
 from SFT.batch_file_gen.upload_batch_file import run as get_results_from_gpt
 import pandas as pd
 
-GPT_BATCH_DIR = "/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/eval_dataset_generation/output_batches/gpt_4/"
-GPT_OUTPUT_DIR = "/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/eval_dataset_generation/gpt4o_outputs"
+# GPT_BATCH_DIR = "/output_batches/gpt_4o/"
+# GPT_OUTPUT_DIR = "/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/eval_dataset_generation/gpt4o_outputs"
+# MODEL = "gpt-4o-2024-08-06"
+# MODEL_NAME = "gpt_4o"
 
+GPT_BATCH_DIR = "/output_batches/gpt_4/"
+GPT_OUTPUT_DIR = "/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/eval_dataset_generation/gpt4_outputs"
+MODEL = "gpt-4-turbo-2024-04-09"
+MODEL_NAME = "gpt_4"
 
-def add_gpt_column_to_eval_dataset(eval_dataset, gpt_outputs_jsonl, model_name="gpt_3_5"):
+def add_gpt_column_to_eval_dataset(eval_dataset, gpt_outputs_jsonl, model_name="gpt_4"):
     answers = []
     with open(gpt_outputs_jsonl, mode='r', encoding='utf-8') as file:
         for line in file:
@@ -27,6 +33,6 @@ def add_gpt_column_to_eval_dataset(eval_dataset, gpt_outputs_jsonl, model_name="
 if __name__ == "__main__":
     eval_dataset = "/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/deep_eval/data/evaluation_dataset.csv"
     questions = pd.read_csv(eval_dataset)["question"].tolist()
-    create_input_batch_files(questions, output_dir=GPT_BATCH_DIR, prefix="eval_dataset_", system_prompt="", model="gpt-4o-2024-08-06")
+    create_input_batch_files(questions, output_dir=GPT_BATCH_DIR, prefix="eval_dataset_", system_prompt="", model=MODEL)
     get_results_from_gpt(gpt_input_batch_dir=GPT_BATCH_DIR, prefix="eval_dataset_", output_dir=GPT_OUTPUT_DIR)
-    add_gpt_column_to_eval_dataset(eval_dataset, f"{GPT_OUTPUT_DIR}/gpt_output_file_0.jsonl", model_name="gpt_4o")
+    add_gpt_column_to_eval_dataset(eval_dataset, f"{GPT_OUTPUT_DIR}/gpt_output_file_0.jsonl", model_name=MODEL_NAME)
