9a7307feb9c089ee5bd4e0dd8959fccdf3fa993c
Mattan Yeroushalmi <mattan.yeroushalmi@similarweb.com>
2024-10-18 12:41:53 +0300

    add explicit completeness metric

--- Benchmarking/deep_eval/g_eval/zemla_metrics.py
+++ Benchmarking/deep_eval/g_eval/zemla_metrics.py
@@ -13,6 +13,17 @@ completeness_metric = GEval(
     evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT]
 )
 
+completeness_metric_explicit = GEval(
+    name="Completeness (Explicit)",
+    evaluation_steps=[
+        "Read and understand the following definition: if an explanation suggests that A causes B, but it is not immediately clear how A causes B, that is considered a gap.",
+        'Given the input question and output model answer, determine if the answer has gaps or not.',
+        "It doesn't matter if the answer or the explanations in it are correct or not. If the answer has any gaps, give it a score of 0. Otherwise give it a score of 10.",
+        "Make sure that the reason you gave for the score doesn't take correctness or accuracy into account, focusing only on gaps in the explanation."
+    ],
+    evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT]
+)
+
 alternatives_metric = GEval(
     name="Lack of Alternatives",
     criteria="There probably aren't many other reasonable alternative explanations to the actual output for this input question.",
