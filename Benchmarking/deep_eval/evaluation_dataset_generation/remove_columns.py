2dcfd0d35a66b1f050f3467a753e972b887460cd
Mattan Yeroushalmi <mattan.yeroushalmi@similarweb.com>
2025-10-18 13:16:28 +0300

    remove bad model

--- Benchmarking/deep_eval/evaluation_dataset_generation/remove_columns.py
+++ Benchmarking/deep_eval/evaluation_dataset_generation/remove_columns.py
@@ -17,12 +17,14 @@ input_path = Path(
     f"{PROJECT_DIR}/Benchmarking/deep_eval/data/test_data/corrected_evaluation_dataset.csv")  # Change this or make it dynamic
 df = pd.read_csv(input_path)
 print(df.columns)
-# df = df.rename(
-#     columns={
-#         "base_model_answer": "llama-2-7b",
-#         "sft_model_answer": "SciComma-2-7b",
-#         "llama70B": "llama-3.3-70b",
-#         "llama70B_SFT": "SciComma-3.3-70B",
-#         "llama3_1_sft": "SciComma-3.1-8B"})
-
+df = df.rename(
+    columns={
+        # "base_model_answer": "llama-2-7b",
+        # "sft_model_answer": "SciComma-2-7b",
+        # "llama70B": "llama-3.3-70b",
+        # "llama70B_SFT": "SciComma-3.3-70B",
+        # "llama3_1_sft": "SciComma-3.1-8B",
+        "scicomma-3.1-dpo_y": "scicomma-3.1-dpo"
+    })
+# df = df.drop(columns=("scicomma-3.1-dpo_x"))
 df.to_csv(input_path, index=False)
--- scripts/add_model_from_csv.py
+++ scripts/add_model_from_csv.py
@@ -1,10 +1,14 @@
 import pandas as pd
+input_path = "/Users/mattan.yeroushalmi/studies/thesis/scripts/generations"
+df = pd.read_csv(f"{input_path}/all_models_joined.csv")
+main_df = pd.read_csv("/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/deep_eval/data/test_data/corrected_evaluation_dataset.csv")
 
-df = pd.read_csv("input1dir/scripts/output_3.csv")
-main_df = pd.read_csv("input2dir/data/test_data/corrected_evaluation_dataset.csv")
 
-
-df = df.rename(columns={"Question": "question", "Answer": "scicomma-3.1-dpo"})
+df = df.rename(columns={
+    "Question": "question",
+    "SciComma-3.1-8B-DPO": "scicomma-3.1-dpo",
+    "SciComma-3.1-8B-DPO_prompt": "scicomma-3.1-dpo_prompt",
+})
 
 # Ensure both question columns have the same data type (string)
 df['question'] = df['question'].astype(str)
@@ -12,4 +16,4 @@ main_df['question'] = main_df['question'].astype(str)
 
 # Use merge instead of join for better control
 joined_df = main_df.merge(df, on="question", how="inner")
-joined_df.to_csv("outputdir/corrected_evaluation_dataset_with_answers_2.csv", index=False)
+joined_df.to_csv("/Users/mattan.yeroushalmi/studies/thesis/Benchmarking/deep_eval/data/test_data/corrected_evaluation_dataset.csv", index=False)
